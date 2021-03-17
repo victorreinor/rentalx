e5ff837788c9158512f85d08f3a30b7551f0b8ce
Victor Reinor <victorreinor@gmail.com>
2021-03-17 19:20:03 -0300

    feat: adiciona principios SOLID, SRP, LSP

---
+++ README.md
@@ -0,0 +1,6 @@
+### SOLID
+S => SRP - Single Responsability Principle (Princípio da Responsabilidade Única);
+O => OCP - Open-Closed Principle (Princípio aberto/fechado);
+L => LSP - Liskov Substituion Principle (Princípio de Substituição de Liskov);
+I => ISP - Interface Segregation Principle (Princípio de Segregação de Interface);
+D => DIP - Dependency Inversion Principle (Princípio de Inversão de Dependência);
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,26 +1,23 @@
 import { Router } from 'express';
 
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CreateCategoryService } from '../services/CreateCategoryService';
 
 const categoriesRoutes = Router();
-const categoriesRepositories = new CategoriesRepository();
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  const categoryAlreadyExists = categoriesRepositories.findByName(name);
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
 
-  if (categoryAlreadyExists) {
-    return response.status(400).json({ error: 'Category Already Exists!' });
-  }
-
-  categoriesRepositories.create({ name, description });
+  createCategoryService.execute({ name, description });
 
   return response.status(201).send();
 });
 
 categoriesRoutes.get('/', (request, response) => {
-  const all = categoriesRepositories.list();
+  const all = categoriesRepository.list();
 
   return response.json(all);
 });
