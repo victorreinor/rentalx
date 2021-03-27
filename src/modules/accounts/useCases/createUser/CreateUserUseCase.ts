c884f396943c18d843f7946129e1457ee3955161
Victor Reinor <victorreinor@gmail.com>
2021-03-27 18:52:45 -0300

    feat: 🎸 adiciona cadastro de usuarios e autenticacao com jwt
    Adiciona cadastro de usuarios e autenticacao com jwt

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,36 @@
+import { hash } from 'bcrypt';
+import { inject, injectable } from 'tsyringe';
+
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+@injectable()
+class CreateUserUseCase {
+  constructor(
+    @inject('UserRepository') private userRepository: IUsersRepository
+  ) {}
+
+  async execute({
+    name,
+    email,
+    driver_license,
+    password,
+  }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.userRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error('User already exists');
+    }
+
+    const passwordHash = await hash(password, 8);
+
+    await this.userRepository.create({
+      name,
+      email,
+      driver_license,
+      password: passwordHash,
+    });
+  }
+}
+
+export { CreateUserUseCase };
