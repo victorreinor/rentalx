2d15bf4fcc888ecec419839b6e025886e0e3af2c
Victor Reinor <victorreinor@gmail.com>
2021-03-31 22:18:10 -0300

    test: 💍 adiciona teste de autenticacao e de criaçao de categori

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,43 @@
+import { AppError } from '../../../../errors/AppError';
+import { CategoriesRepositoryInMemory } from '../../repositories/in-memory/CategoriesRepositoryInMemory';
+import { CreateCategoryUseCase } from './CreateCategoryUseCase';
+
+let createCategoryUseCase: CreateCategoryUseCase;
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+
+describe('Create Category', () => {
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+    createCategoryUseCase = new CreateCategoryUseCase(
+      categoriesRepositoryInMemory
+    );
+  });
+
+  it('should be able to create a new category', async () => {
+    const category = {
+      name: 'Category Teste',
+      description: 'Category description teste',
+    };
+    await createCategoryUseCase.execute(category);
+
+    const categoryCreated = await categoriesRepositoryInMemory.findByName(
+      category.name
+    );
+
+    expect(categoryCreated).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new category with name exists', async () => {
+    expect(async () => {
+      const category = {
+        name: 'Category Teste',
+        description: 'Category description teste',
+      };
+
+      await createCategoryUseCase.execute(category);
+      await createCategoryUseCase.execute(category);
+
+      await categoriesRepositoryInMemory.findByName(category.name);
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
