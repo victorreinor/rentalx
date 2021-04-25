e8e4a253ccbfb3932700c9945ab02f120bc8fc6d
Victor Reinor <victorreinor@gmail.com>
2021-04-25 16:45:39 -0300

    test: 💍 adiciona testes integrados

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,3 +1,15 @@
-import { createConnection } from 'typeorm';
+import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-createConnection();
+export default async (): Promise<Connection> => {
+  const defaultOptions = await getConnectionOptions();
+
+  return createConnection(
+    Object.assign(defaultOptions, {
+      host: '192.168.100.3',
+      database:
+        process.env.NODE_ENV === 'test'
+          ? 'rentx_test'
+          : defaultOptions.database,
+    })
+  );
+};
